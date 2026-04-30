3dd96f102e08b0233c51595f0837e126fe2f3b2f
cbig3 <romanvillagran386@gmail.com>
2026-04-30 14:15:07 -0700

    added the code to movie actors and also started on the json

--- Comp003B.Assignment 6/Models/MovieActors.cs
+++ Comp003B.Assignment 6/Models/MovieActors.cs
@@ -2,6 +2,14 @@
 {
     public class MovieActors
     {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+
+        public int ActorId { get; set; }
         
+
+        public virtual Actors? Actors { get; set; }
+        public virtual Movie? Movie { get; set; }
+
     }
 }
