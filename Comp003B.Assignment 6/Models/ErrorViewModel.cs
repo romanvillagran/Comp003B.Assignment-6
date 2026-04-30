fb2f8abf17e3f167f6e38563162e3d01628cb0ef
cbig3 <romanvillagran386@gmail.com>
2026-04-30 13:07:29 -0700

    Add project files.

---
+++ Comp003B.Assignment 6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comp003B.Assignment_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Comp003B.Assignment 6/Models/Movie.cs
@@ -0,0 +1,6 @@
+﻿namespace Comp003B.Assignment_6.Models
+{
+    public class Movie
+    {
+    }
+}
