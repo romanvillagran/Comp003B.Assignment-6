15394057a6db9d1d01bda8f7a4d853ed5ab81d67
cbig3 <romanvillagran386@gmail.com>
2026-04-30 22:44:00 -0700

    added the code to the moviedatabase context file

--- Comp003B.Assignment 6/Data/MovieDatabaseContext.cs
+++ Comp003B.Assignment 6/Data/MovieDatabaseContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Comp003B.Assignment_6.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Comp003B.Assignment_6.Data
 {
     public class MovieDatabaseContext : DbContext
     {
+        public MovieDatabaseContext(DbContextOptions<MovieDatabaseContext> options) : base(options) {
+        }
 
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actors> Actors { get; set; }
+        public DbSet<MovieActors> MovieActors { get; set; }
     }
 }
